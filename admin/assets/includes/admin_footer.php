405718013da7fc8f91352f994e2549375e118ef2
Nithin_Punem <149465884+NithinPunem@users.noreply.github.com>
2025-02-13 22:16:11 +0530

    Add files via upload

---
+++ admin/assets/includes/admin_footer.php
@@ -0,0 +1,37 @@
+<html>
+
+<body>
+    <script src="assets/js/jquery.min.js"></script>
+    <script src="assets/js/jquery-1.12.4.min.js"></script>
+  
+    <script src="assets/js/popper.min.js"></script>
+    <script src="assets/js/bootstrap.min.js"></script>
+    <script src="assets/js/sweetalert.js"></script>
+    <script src="assets/js/progressbar.min.js"></script>
+    <script src="assets/js/charts/jquery.flot.min.js"></script>
+    <script src="assets/js/charts/jquery.flot.pie.min.js"></script>
+    <script src="assets/js/charts/jquery.flot.categories.min.js"></script>
+    <script src="assets/js/charts/jquery.flot.stack.min.js"></script>
+    <script src="assets/js/charts/chart.min.js"></script>
+    <script src="assets/js/charts/chartist.min.js"></script>
+    <script src="assets/js/charts/chartist-data.js"></script>
+    <script src="assets/js/charts/demo.js"></script>
+    <script src="assets/js/maps/jquery-jvectormap-2.0.2.min.js"></script>
+    <script src="assets/js/maps/jquery-jvectormap-world-mill-en.js"></script>
+    <script src="assets/js/maps/jvector-maps.js"></script>
+    <script src="assets/js/charts/canvas.min.js"></script>
+    <script src="assets/js/calendar/bootstrap_calendar.js"></script>
+    <script src="assets/js/calendar/demo.js"></script>
+    <script src="assets/js/jquery.nice-select.min.js"></script>
+    <script src="assets/js/jquery.dataTables.min.js"></script>
+    <script src="assets/js/dataTables.bootstrap4.min.js"></script>
+    <script src="assets/js/jsgrid.min.js"></script>
+    <script src="assets/js/jsgrid-demo.php"></script>
+
+    <script src="assets/js/custom.js"></script>
+    <script>
+        $('.bulk-actions').niceSelect();
+    </script>
+</body>
+
+</html>
